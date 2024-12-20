18899f90da16c347765cb31f069c6a255cbbb517
yuvaraju1 <yuvaraju.dasari@talakunchi.com>
2024-12-20 10:11:07 +0530

    Create main.tf

---
+++ terraform-example/main.tf
@@ -0,0 +1,18 @@
+# main.tf
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
