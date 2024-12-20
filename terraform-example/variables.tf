c9d4406fe313af31d1cfdd3953c5c8e89a6abd4f
yuvaraju1 <yuvaraju.dasari@talakunchi.com>
2024-12-20 10:11:34 +0530

    Create variables.tf

---
+++ terraform-example/variables.tf
@@ -0,0 +1,18 @@
+# variables.tf
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Type of the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
+}
